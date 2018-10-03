dd934e802e20570c85fc434760dd1a6638218615
Chane13M <chane13.m@gmail.com>
2018-10-03 15:35:00 +0200

    minor changes

--- root_analysis/multiple_positions_of_peaks.C
+++ root_analysis/multiple_positions_of_peaks.C
@@ -6,21 +6,35 @@
     //////////////////////////////////////////////////////////////////////////////////////////////////////
   char name[128];
 
-  int nrofpeaks=2;
+  int nrofpeaks=10;
   Float_t peakposition[nrofpeaks];
   Float_t sigma[nrofpeaks];
   Float_t low[nrofpeaks];
   Float_t high[nrofpeaks];
   Float_t ExNNDC[nrofpeaks];
 
-  low[0]=680;     high[0]=686;
-  low[1]=571.5;   high[1]=579;
-  //low[2]=495;   high[2]=505;
+  low[0]=680;   high[0]=686; //9.305 MeV
+  low[1]=648;   high[1]=652;
+  low[2]=637;   high[2]=642;
+  low[3]=612;   high[3]=617;
+  low[4]=606;   high[4]=611; //11.278 MeV
+  low[5]=601;   high[5]=606;
+  low[6]=538;   high[6]=550;
+  low[7]=495;   high[7]=505; //15.25 MeV
+  low[8]=480;   high[8]=488;
+  low[9]=442;   high[9]=454;
 
   ExNNDC[0]=9.30539;
   ExNNDC[1]=11.7281;
-  //ExNNDC[2]=15.25;
-    
+  ExNNDC[2]=15.25;
+  ExNNDC[3]=8;
+  ExNNDC[4]=7;
+  ExNNDC[5]=6;
+  ExNNDC[6]=5;
+  ExNNDC[7]=4;
+  ExNNDC[8]=3;
+  ExNNDC[9]=2;
+
   for(Int_t i=0;i<nrofpeaks;i++){
     sprintf(name,"hExdiff->Fit(\"gaus\",\"\",\"\",%f,%f);",low[i],high[i]);
     gROOT->ProcessLine(name);
