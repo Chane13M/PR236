841ae1dbd4490eac4c6884567dfa2de40a6e3d88
Chane13M <chane13.m@gmail.com>
2018-10-24 19:40:00 +0200

    Cross section code added

--- root_analysis/BackgroundSubtract_CrossSections.C
+++ root_analysis/BackgroundSubtract_CrossSections.C
@@ -1,5 +1,4 @@
-
-{
+{ 
 gStyle->SetPalette(1,0);
 
 c3 = new TCanvas("c3","c3",800,10,1000,600);
@@ -44,10 +43,10 @@ hExdiff->GetXaxis()->SetTitleOffset(1.1);
 hExdiff->GetYaxis()->SetTitleOffset(1.4); 
 
 hExdiff->Add(hEx);
-hExdiff->Add(hExback1, -1);//,-0.416667);
-hExdiff->Add(hExback2, -1);//,-0.416667);
-hExbacktot->Add(hExback1);//,0.416667);
-hExbacktot->Add(hExback2);//,0.416667);
+hExdiff->Add(hExback1, -1);
+hExdiff->Add(hExback2, -1);
+hExbacktot->Add(hExback1);
+hExbacktot->Add(hExback2);
 
 hEx->SetLineColor(4);
 hExback1->SetLineColor(2);
@@ -71,4 +70,52 @@ leg->AddEntry(hExdiff,"Background subtracted data","l");
 //leg->SetLegendFont(16);
 leg->Draw();
 
+//==============================================
+// Uncomment the following to obtain double differential cross sections 
+ /* 
+TH1F *hExCs = new TH1F("hExCs","^{150}Sm Cross-section Excitation energy",3100,-1.,30.);
+hExCs->Add(hExdiff);
+hExCs->GetXaxis()->SetTitle("E_{x} (MeV)");
+hExCs->GetXaxis()->CenterTitle(true);
+hExCs->GetYaxis()->SetTitle("(mb/sr.MeV)");
+hExCs->GetYaxis()->CenterTitle(true);
+SA = 3.48; // msr  
+cii = 8062253; 
+tgtthick = 3.76; // mg/cm^2 
+A=150;
+e=1.60217733;
+eff=0.7063;
+range=6;
+NA=6.0221367;
+elecdead=0.9935; 
+ebin=0.01;
+incidentflux=(cii*range)/e;
+scatterers=(tgtthick*NA)/A;
+temp=elecdead*eff*ebin*SA;
+norm=incidentflux*scatterers*temp;
+cout <<"cii = " << cii << endl;
+cout <<"Incident flux = "<< incidentflux << endl;
+cout <<"scatterers = "<< scatterers << endl;
+cout <<"temp = "<< temp << endl;
+hExCs->Scale(1000/norm);
+hExCs->Draw();
+//==================================================
+*/
+
+//TH1F *hExSm150 = new TH1F("hExSm150","Excitation energy",3100,-1.,30.);
+//hExSm150 = hExdiff;
+
+//Output a data file in PWD
+
+/*
+ ofstream out;
+  out.open("datafile-150Sm-ExCs-pExCalib");
+  float lut;
+  Int_t nbins = hExCs->GetNbinsX();
+  // Now write to a datafile
+  for(Int_t i=0;i<nbins;i++) {
+    out << i << "  " << hExCs->GetBinContent(i)  << endl;
+  }
+  out.close();
+*/
 }
